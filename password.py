decf36b78e32ded1df8332cc87119d72e8d4e540
Herman <herman.mugu@voco.ee>
2026-01-16 18:46:09 +0200

    lisasin vahe mis pikkusega uusparool võib olla

--- password.py
+++ password.py
@@ -2,3 +2,8 @@
 
 vanaparool = input("Sisesta oma praegune parool: ")
 uusparool = input("Sisesta oma uus parool: ")
+
+"""kontrollin kas parool on 6ige pikkusega(8-64)"""
+
+if len(uusparool) < 8 or len(uusparool) > 64:
+     print("parool on vale pikkusega")
